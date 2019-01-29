4beae7f6508dd2041a5224c97dd654941eb71d00
Erik Leo <eleo7@bloomberg.net>
2019-01-29 11:08:26 -0500

    jest basic tests

---
+++ test/index1.test.js
@@ -0,0 +1,5 @@
+describe("test main", () => {
+    test("do simple test 1", () => {
+        expect(1).toBe(1);
+    })
+})
---
+++ test/index2.test.js
@@ -0,0 +1,5 @@
+describe("test main", () => {
+    test("do simple test 2", () => {
+        expect(2).toBe(2);
+    })
+})
